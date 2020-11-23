601d233628715ca5de01d730b91aa4a807f23c5f
Ollie Robins <ollierobins93@gmail.com>
2020-11-23 11:37:17 +0000

    first commit

---
+++ database.js
@@ -0,0 +1,73 @@
+// Use ES6 Imports for mongodb and our mongo client
+const mongodb = require('mongodb')
+const mongoClient = mongodb.MongoClient;
+// ************************************************
+// HEY YOU! REPLACE "andys_db" below with the name of your database!!!!
+// ************************************************
+let dbName = "_golfCourses";
+// connection string
+const uri = "mongodb+srv://thisisatest:thisisatest@cluster0.lsfrb.mongodb.net/<dbname>?retryWrites=true&w=majority";
+const getCollectionDocuments = async (collectionName) => {
+    // Connect to our database / open our connection
+    const mongo = await mongoClient.connect(uri, { useUnifiedTopology: true })
+    // Retrieve our collection
+    const dataCollection = await mongo.db(dbName).collection(collectionName).find({}).toArray();
+    // Close our connection
+    mongo.close();
+    return dataCollection;
+}
+const createCollectionDocument = async (collectionName, data) => {
+    // Connect to our database / open our connection
+    const mongo = await mongoClient.connect(uri, { useUnifiedTopology: true })
+    // Create our document
+    if (!data._id) {
+        data._id = new mongodb.ObjectID().toString();
+        await mongo.db(dbName).collection(collectionName).insertOne(data)
+    } else {
+        updateCollectionDocument(collectionName, data);
+    }
+    // Close our connection
+    mongo.close();
+}
+const updateCollectionDocument = async (collectionName, data) => {
+    // Connect to our database / open our connection
+    const mongo = await mongoClient.connect(uri, { useUnifiedTopology: true })
+    // Retrieve our collection
+    var myquery = { _id: new mongodb.ObjectID(data._id) };
+    var newvalues = { $set: data };
+    await mongo.db(dbName).collection(collectionName).replaceOne(
+        { _id : data._id },
+        data, 
+        { upsert: true} 
+    );
+    // Close our connection
+    mongo.close();
+}
+const deleteCollectionDocument = async (collectionName, data) => {
+    // Connect to our database / open our connection
+    const mongo = await mongoClient.connect(uri, { useUnifiedTopology: true })
+    // Retrieve our collection
+    await mongo.db(dbName).collection(collectionName).deleteOne(
+        { _id : data._id }
+    );
+    // Close our connection
+    mongo.close();
+}
+
+module.exports.getCollectionDocuments = getCollectionDocuments;
+module.exports.createCollectionDocument = createCollectionDocument;
+module.exports.updateCollectionDocument = updateCollectionDocument;
+module.exports.deleteCollectionDocument = deleteCollectionDocument;
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ index.js
@@ -0,0 +1,63 @@
+//import http from 'http';
+const http = require('http');
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser')
+const {getCollectionDocuments, createCollectionDocument, deleteCollectionDocument} = require('./database.js');
+
+
+
+//create our express api
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json())
+
+
+//store the users in array for now, it should be mongo DB
+
+    //listen for request to the endpoint /something and return with a message
+    app.get('/', async (request, response) => {
+        //read from our users array
+        const courses = await getCollectionDocuments('data');
+        response.send(courses)
+})
+
+    //create a POST endpoint
+    app.post('/create', async (request, response) => {
+        const newCourse = request.body
+        console.log(request.body)
+        await createCollectionDocument('data' , newCourse)
+        //save it to the database
+        response.send({name:"we created this user"})
+    })
+
+    //create a delete endpoint
+    app.delete('/delete', async (request, response) => {
+        //i need to delete my user from the array
+        const CourseToDelete = request.body
+        //take out the user we are trying to delete
+        await deleteCollectionDocument('data', CourseToDelete)
+
+        response.send({name: 'we deleted this user'})
+    })
+
+
+    //listen on port 8090
+    app.listen(8090);
+
+
+// // this is going to be our node api using vainllah JS :)
+// const server = http.createServer( (request, response) => {
+    
+    
+//     //challenge: return a different responsed based on what the urls is in the request
+//     if (request.url == '/something') {        
+//         // we need to send something in the response
+//         response.end('hello from my node api i just created :) ')
+//     } else {
+//         response.end('hello from my catch all endpoints ')
+//     };   
+// })
+// //give the server a port to listen on
+// server.listen(8090);
