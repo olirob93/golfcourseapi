b0ec9ff925fb954cdc4334e58ad430f38ead7d10
Ollie Robins <ollierobins93@gmail.com>
2020-11-24 15:10:55 +0000

    Latest heroku config

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ app.use(bodyParser.json())
 
 
     //listen on port 8090
-    app.listen('https://nameless-wildwood-48294.herokuapp.com/');
+    app.listen(8090);
 
 
 // // this is going to be our node api using vanilla JS :)
